bcee5fbcf2b5cdced009cec6fa85b27d4872b041
Andrey Sokolov <Andrey.Sokolov@jetbrains.com>
2015-02-13 15:02:26 +0300

    (phpstorm-stubs)PHP Fixed stubs WI-26454 Missing autocomplete for opcache

--- OPcache.php
+++ OPcache.php
@@ -32,3 +32,20 @@ function opcache_invalidate($script, $force = FALSE) { }
  * @return boolean Returns <b>TRUE</b> if the opcode cache was reset, or <b>FALSE</b> if the opcode cache is disabled.
  */
 function opcache_reset() { }
+
+/**
+ * (PHP 5 &gt;= 5.5.5, PECL ZendOpcache &gt;= 7.0.2 )<br/>
+ * Get status information about the cache
+ * @link http://php.net/manual/en/function.opcache-get-status.php
+ * @param bool $get_scripts <p>Include script specific state information</p>
+ * @return array <p>Returns an array of information, optionally containing script specific state information</p>
+ */
+function opcache_get_status ($get_scripts = TRUE) {}
+
+/**
+ * (PHP 5 &gt;= 5.5.5, PECL ZendOpcache &gt;= 7.0.2 )<br/>
+ * Get configuration information about the cache
+ * @link http://php.net/manual/en/function.opcache-get-configuration.php
+ * @return array <p>Returns an array of information, including ini, blacklist and version</p>
+ */
+function opcache_get_configuration() {}
