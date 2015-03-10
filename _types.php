84933e260bcf42e14d57415a440b726813264775
Alexey Gopachenko <neuro159@gmail.com>
2015-03-10 11:35:20 +0100

    (phpstorm-stubs)PHP WI-26712 __debugInfo missing from override window

--- _types.php
+++ _types.php
@@ -191,6 +191,16 @@ class object {
    */
   function __invoke() {}
 
+    /**
+     * @since PHP 5.6.0
+     * This method is called by var_dump() when dumping an object to get the properties that should be shown.
+     * If the method isn't defined on an object, then all public, protected and private properties will be shown.
+     *
+     * @return array
+     * @link http://php.net/manual/en/language.oop5.magic.php#language.oop5.magic.debuginfo
+     */
+    function __debugInfo(){}
+
   /**
    * This static method is called for classes exported by var_export() since PHP 5.1.0.
    * The only parameter of this method is an array containing exported properties in the form array('property' => value, ...).
