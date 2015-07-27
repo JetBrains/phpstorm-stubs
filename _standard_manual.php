8190ff36b5db165798a5c3c948f40164a754ee85
Alexey Gopachenko <neuro159@gmail.com>
2015-07-27 11:00:48 +0200

    PHP WI-28210 Stub: incorrect return type for fastcgi_finish_request()

--- _standard_manual.php
+++ _standard_manual.php
@@ -38,7 +38,10 @@ define("__COMPILER_HALT_OFFSET__",0);
 function hex2bin($data) {};
 
 /**
- * special function to finish request and flush all data while continuing to do something time-consuming (video converting, stats processing etc.);
+ * @since 5.3.3
+ * This function flushes all response data to the client and finishes the request.
+ * This allows for time consuming tasks to be performed without leaving the connection to the client open.
+ * @return boolean Returns TRUE on success or FALSE on failure.
  * @link http://php.net/manual/en/install.fpm.php
  */
 function fastcgi_finish_request() {};
