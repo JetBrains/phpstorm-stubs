da4579f9c799af202bd57bc495868c614ff16ef7
Alexey Gopachenko <neuro159@gmail.com>
2015-12-22 11:03:42 +0100

    PHP WI-30108 Misleading tooltip for the "strval" function

--- standard_5.php
+++ standard_5.php
@@ -66,8 +66,8 @@ function doubleval ($var) {}
  * The variable that is being converted to a string.
  * </p>
  * <p>
- * var may be any scalar type. You cannot use
- * strval on arrays or objects.
+ * $var may be any scalar type or an object that implements the __toString() method. 
+ * You cannot use strval() on arrays or objects that do not implement the __toString() method.
  * </p>
  * @return string The string value of var.
  * @since 4.0
