85078e669e2f46e4d660fa43acdee3477f36af78
Alexey Gopachenko <neuro159@gmail.com>
2016-07-26 14:22:10 +0200

    PHP WI-32596 array_map Invalid return type type inffered

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -43,7 +43,7 @@ namespace PHPSTORM_META {
   override(\array_shift(0), elementType(0));
   override(\array_reverse(0), elementType(0));
   override(\array_pop(0), elementType(0));
-  override(\array_map(0), type(0));
+//  override(\array_map(0), type(2));
   override(\array_filter(0), type(0));
   override(\array_reduce(0), elementType(0));
 
