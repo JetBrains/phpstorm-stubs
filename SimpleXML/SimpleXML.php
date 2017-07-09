0ad4f840b9288c7e229da81ebd96ed7007863c2a
Benjamin Morel <benjamin.morel@gmail.com>
2017-07-09 11:46:36 +0200

    SimpleXMLElement accessor always returns a single SimpleXMLElement object

--- SimpleXML/SimpleXML.php
+++ SimpleXML/SimpleXML.php
@@ -24,7 +24,7 @@ class SimpleXMLElement implements Traversable {
 	/**
      * Provides access to element's children
      * @param $name child name
-     * @return SimpleXMLElement[]
+     * @return SimpleXMLElement
      */
     function __get($name) {}
 
