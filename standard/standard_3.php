3d32a87feff2fd3c1966fe7237e1cf1978ebd7b9
Ilya Konstantantinov <ilya.konstantinov@gmail.com>
2017-07-14 06:11:26 -0700

    Update getenv (#237)
    * Update getenv
    
    Adding optional $local_only argument, removing incorrect documentation claiming `getenv()` returns an array of variables.
    
    * Update standard_3.php
    
    * Update standard_3.php
    
    * Update standard_3.php

--- standard/standard_3.php
+++ standard/standard_3.php
@@ -778,14 +778,19 @@ function long2ip ($proper_address) {}
  * @param string $varname [optional] <p>
  * The variable name.
  * </p>
+ * @param bool $local_only [optional] <p>
+ * Set to true to only return local environment variables (set by the operating system or putenv).
+ * </p>
  * @return string|array|false the value of the environment variable
- * varname or array with all environment variables if no variable name
- * provided, or false on an error.
+ * varname or an associative array with all environment variables if no variable name
+ * is provided, or false on an error.
  * @since 4.0
- * @since 5.0
- * @since 7.1
+ * @since 5.5.38 The local_only parameter has been added.
+ * @since 5.6.24 The local_only parameter has been added.
+ * @since 7.0.9 The local_only parameter has been added.
+ * @since 7.1 The varname parameter was made optional.
  */
-function getenv ($varname = null) {}
+function getenv ($varname = null, $local_only = false) {}
 
 /**
  * Sets the value of an environment variable
