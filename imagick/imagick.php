96295fdb21004f28e24915d9355adc38fdce6984
Ali Alwash <alitrix@gmail.com>
2017-10-26 10:59:31 +0200

    Fixed all indenting to tabs WI-38010

--- imagick/imagick.php
+++ imagick/imagick.php
@@ -723,17 +723,17 @@ class Imagick implements Iterator, Countable {
 
 	/**
 	 * Adaptively resize image with data dependent triangulation
-     *
+	 *
 	 * If legacy is true, the calculations are done with the small rounding bug that existed in Imagick before 3.4.0.<br>
 	 * If false, the calculations should produce the same results as ImageMagick CLI does.<br>
-     * <br>
+	 * <br>
 	 * <b>Note:</b> The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched.
 	 * In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
 	 * @link http://php.net/manual/en/imagick.adaptiveresizeimage.php
 	 * @param int $columns The number of columns in the scaled image.
 	 * @param int $rows The number of rows in the scaled image.
 	 * @param bool $bestfit [optional] Whether to fit the image inside a bounding box.<br>
-     *                                 The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched. In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
+	 *                                 The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched. In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
 	 * @param bool $legacy [optional] Added since 3.4.0. Default value FALSE
 	 * @return bool TRUE on success
 	 * @throws ImagickException Throws ImagickException on error
@@ -1902,7 +1902,7 @@ class Imagick implements Iterator, Countable {
 	 * @param bool $bestfit [optional] <p>
 	 * Whether to force maximum values
 	 * </p>
-     * The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched. In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
+	 * The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched. In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
 	 * @param bool $fill [optional]
 	 * @param bool $legacy [optional] Added since 3.4.0. Default value FALSE
 	 * @return bool <b>TRUE</b> on success.
@@ -2404,10 +2404,10 @@ class Imagick implements Iterator, Countable {
 	 * Scales an image to the desired dimensions with one of these filters:<br>
 	 * If legacy is true, the calculations are done with the small rounding bug that existed in Imagick before 3.4.0.<br>
 	 * If false, the calculations should produce the same results as ImageMagick CLI does.<br>
-     * <br>
-     * <b>Note:</b> The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched.<br>
-     * In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
-     * @link http://php.net/manual/en/imagick.resizeimage.php
+	 * <br>
+	 * <b>Note:</b> The behavior of the parameter bestfit changed in Imagick 3.0.0. Before this version given dimensions 400x400 an image of dimensions 200x150 would be left untouched.<br>
+	 * In Imagick 3.0.0 and later the image would be scaled up to size 400x300 as this is the "best fit" for the given dimensions. If bestfit parameter is used both width and height must be given.
+	 * @link http://php.net/manual/en/imagick.resizeimage.php
 	 * @param int $columns Width of the image
 	 * @param int $rows Height of the image
 	 * @param int $filter Refer to the list of filter constants.
@@ -4750,20 +4750,20 @@ class Imagick implements Iterator, Countable {
 	 */
 	public function evaluateImages ($EVALUATE_CONSTANT) { }
 
-    /**
-     * Extracts the 'mean' from the image and adjust the image to try make set its gamma appropriately.
-     * @param int $channel [optional] Default value Imagick::CHANNEL_ALL
-     * @return bool
-     * @since 3.4.1
-     */
-    public function autoGammaImage ($channel = Imagick::CHANNEL_ALL) { }
+	/**
+	 * Extracts the 'mean' from the image and adjust the image to try make set its gamma appropriately.
+	 * @param int $channel [optional] Default value Imagick::CHANNEL_ALL
+	 * @return bool
+	 * @since 3.4.1
+	 */
+	public function autoGammaImage ($channel = Imagick::CHANNEL_ALL) { }
 
-    /**
-     * Adjusts an image so that its orientation is suitable $ for viewing (i.e. top-left orientation).
-     * @return bool
-     * @since 3.4.1
-     */
-    public function autoOrient () { }
+	/**
+	 * Adjusts an image so that its orientation is suitable $ for viewing (i.e. top-left orientation).
+	 * @return bool
+	 * @since 3.4.1
+	 */
+	public function autoOrient () { }
 }
 
 /**
@@ -6551,70 +6551,70 @@ class ImagickPixel  {
  * @link http://php.net/manual/en/class.imagickkernel.php
  */
 class ImagickKernel {
-    /**
-     * Attach another kernel to this kernel to allow them to both be applied in a single morphology or filter function. Returns the new combined kernel.
-     * @link http://php.net/manual/en/imagickkernel.addkernel.php
-     * @param ImagickKernel $imagickKernel
-     * @return void
-     * @since 3.3.0
-     */
-    public function addKernel(ImagickKernel $imagickKernel) { }
+	/**
+	 * Attach another kernel to this kernel to allow them to both be applied in a single morphology or filter function. Returns the new combined kernel.
+	 * @link http://php.net/manual/en/imagickkernel.addkernel.php
+	 * @param ImagickKernel $imagickKernel
+	 * @return void
+	 * @since 3.3.0
+	 */
+	public function addKernel(ImagickKernel $imagickKernel) { }
 
-    /**
-     * Adds a given amount of the 'Unity' Convolution Kernel to the given pre-scaled and normalized Kernel. This in effect adds that amount of the original image into the resulting convolution kernel. The resulting effect is to convert the defined kernels into blended soft-blurs, unsharp kernels or into sharpening kernels.
-     * @link http://php.net/manual/en/imagickkernel.addunitykernel.php
-     * @return void
-     * @since 3.3.0
-     */
-    public function addUnityKernel() { }
+	/**
+	 * Adds a given amount of the 'Unity' Convolution Kernel to the given pre-scaled and normalized Kernel. This in effect adds that amount of the original image into the resulting convolution kernel. The resulting effect is to convert the defined kernels into blended soft-blurs, unsharp kernels or into sharpening kernels.
+	 * @link http://php.net/manual/en/imagickkernel.addunitykernel.php
+	 * @return void
+	 * @since 3.3.0
+	 */
+	public function addUnityKernel() { }
 
-    /**
-     * Create a kernel from a builtin in kernel. See http://www.imagemagick.org/Usage/morphology/#kernel for examples.<br>
-     * Currently the 'rotation' symbols are not supported. Example: $diamondKernel = ImagickKernel::fromBuiltIn(\Imagick::KERNEL_DIAMOND, "2");
-     * @link http://php.net/manual/en/imagickkernel.frombuiltin.php
-     * @param string $kernelType The type of kernel to build e.g. \Imagick::KERNEL_DIAMOND
-     * @param string $kernelString A string that describes the parameters e.g. "4,2.5"
-     * @return void
-     * @since 3.3.0
-     */
-    public static function fromBuiltin($kernelType, $kernelString) { }
+	/**
+	 * Create a kernel from a builtin in kernel. See http://www.imagemagick.org/Usage/morphology/#kernel for examples.<br>
+	 * Currently the 'rotation' symbols are not supported. Example: $diamondKernel = ImagickKernel::fromBuiltIn(\Imagick::KERNEL_DIAMOND, "2");
+	 * @link http://php.net/manual/en/imagickkernel.frombuiltin.php
+	 * @param string $kernelType The type of kernel to build e.g. \Imagick::KERNEL_DIAMOND
+	 * @param string $kernelString A string that describes the parameters e.g. "4,2.5"
+	 * @return void
+	 * @since 3.3.0
+	 */
+	public static function fromBuiltin($kernelType, $kernelString) { }
 
-    /**
-     * Create a kernel from a builtin in kernel. See http://www.imagemagick.org/Usage/morphology/#kernel for examples.<br>
-     * Currently the 'rotation' symbols are not supported. Example: $diamondKernel = ImagickKernel::fromBuiltIn(\Imagick::KERNEL_DIAMOND, "2");
-     * @link http://php.net/manual/en/imagickkernel.frombuiltin.php
-     * @see http://www.imagemagick.org/Usage/morphology/#kernel
-     * @param array $matrix A matrix (i.e. 2d array) of values that define the kernel. Each element should be either a float value, or FALSE if that element shouldn't be used by the kernel.
-     * @param array $origin [optional] Which element of the kernel should be used as the origin pixel. e.g. For a 3x3 matrix specifying the origin as [2, 2] would specify that the bottom right element should be the origin pixel.
-     * @return ImagickKernel
-     * @since 3.3.0
-     */
-    public static function fromMatrix($matrix, $origin) { }
+	/**
+	 * Create a kernel from a builtin in kernel. See http://www.imagemagick.org/Usage/morphology/#kernel for examples.<br>
+	 * Currently the 'rotation' symbols are not supported. Example: $diamondKernel = ImagickKernel::fromBuiltIn(\Imagick::KERNEL_DIAMOND, "2");
+	 * @link http://php.net/manual/en/imagickkernel.frombuiltin.php
+	 * @see http://www.imagemagick.org/Usage/morphology/#kernel
+	 * @param array $matrix A matrix (i.e. 2d array) of values that define the kernel. Each element should be either a float value, or FALSE if that element shouldn't be used by the kernel.
+	 * @param array $origin [optional] Which element of the kernel should be used as the origin pixel. e.g. For a 3x3 matrix specifying the origin as [2, 2] would specify that the bottom right element should be the origin pixel.
+	 * @return ImagickKernel
+	 * @since 3.3.0
+	 */
+	public static function fromMatrix($matrix, $origin) { }
 
-    /**
-     * Get the 2d matrix of values used in this kernel. The elements are either float for elements that are used or 'false' if the element should be skipped.
-     * @link http://php.net/manual/en/imagickkernel.getmatrix.php
-     * @return array A matrix (2d array) of the values that represent the kernel.
-     * @since 3.3.0
-     */
-    public function getMatrix() { }
+	/**
+	 * Get the 2d matrix of values used in this kernel. The elements are either float for elements that are used or 'false' if the element should be skipped.
+	 * @link http://php.net/manual/en/imagickkernel.getmatrix.php
+	 * @return array A matrix (2d array) of the values that represent the kernel.
+	 * @since 3.3.0
+	 */
+	public function getMatrix() { }
 
-    /**
-     * ScaleKernelInfo() scales the given kernel list by the given amount, with or without normalization of the sum of the kernel values (as per given flags).<br>
-     * The exact behaviour of this function depends on the normalization type being used please see http://www.imagemagick.org/api/morphology.php#ScaleKernelInfo for details.<br>
-     * Flag should be one of Imagick::NORMALIZE_KERNEL_VALUE, Imagick::NORMALIZE_KERNEL_CORRELATE, Imagick::NORMALIZE_KERNEL_PERCENT or not set.
-     * @link http://php.net/manual/en/imagickkernel.scale.php
-     * @see http://www.imagemagick.org/api/morphology.php#ScaleKernelInfo
-     * @return void
-     * @since 3.3.0
-     */
-    public function scale() { }
+	/**
+	 * ScaleKernelInfo() scales the given kernel list by the given amount, with or without normalization of the sum of the kernel values (as per given flags).<br>
+	 * The exact behaviour of this function depends on the normalization type being used please see http://www.imagemagick.org/api/morphology.php#ScaleKernelInfo for details.<br>
+	 * Flag should be one of Imagick::NORMALIZE_KERNEL_VALUE, Imagick::NORMALIZE_KERNEL_CORRELATE, Imagick::NORMALIZE_KERNEL_PERCENT or not set.
+	 * @link http://php.net/manual/en/imagickkernel.scale.php
+	 * @see http://www.imagemagick.org/api/morphology.php#ScaleKernelInfo
+	 * @return void
+	 * @since 3.3.0
+	 */
+	public function scale() { }
 
-    /**
-     * Separates a linked set of kernels and returns an array of ImagickKernels.
-     * @link http://php.net/manual/en/imagickkernel.separate.php
-     * @return void
-     * @since 3.3.0
-     */
-    public function seperate() { }
+	/**
+	 * Separates a linked set of kernels and returns an array of ImagickKernels.
+	 * @link http://php.net/manual/en/imagickkernel.separate.php
+	 * @return void
+	 * @since 3.3.0
+	 */
+	public function seperate() { }
 }
