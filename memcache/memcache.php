a66270ab19188436413d61b042c438607eb13cce
Charles Peterson <cpeterson@brokerbin.com>
2017-11-10 12:27:36 -0600

    fix sorting.

--- memcache/memcache.php
+++ memcache/memcache.php
@@ -384,7 +384,13 @@ class Memcache extends MemcachePool  {
  */
 function memcache_connect ($host, $port, $timeout = 1) {}
 
-function memcache_pconnect () {}
+/**
+ * @param      $host
+ * @param null $port
+ * @param int  $timeout
+ * @return Memcache
+ */
+function memcache_pconnect ($host, $port=null, $timeout=1) {}
 
 function memcache_add_server () {}
 
