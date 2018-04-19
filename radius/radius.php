42cf8e236a2079cec9757d96213de8ea11c166d0
Ali Alwash <alitrix@gmail.com>
2018-04-19 10:17:16 +0200

    WI-38483 Remove typehing

--- radius/radius.php
+++ radius/radius.php
@@ -341,7 +341,7 @@ function radius_acct_open() { }
  * @see radius_config()
  * @since 1.1.0
  */
-function radius_add_server(resource $radius_handle , $hostname, $port , $secret, $timeout, $max_tries) { }
+function radius_add_server($radius_handle , $hostname, $port , $secret, $timeout, $max_tries) { }
 
 /**
  * Creates a Radius handle for authentication
@@ -358,7 +358,7 @@ function radius_auth_open() { }
  * @return bool Returns <b>TRUE</b> on success or <b>FALSE</b> on failure.
  * @since 1.1.0
  */
-function radius_close(resource $radius_handle) { }
+function radius_close($radius_handle) { }
 
 /**
  * Before issuing any Radius requests, the library must be made aware of the servers it can contact. The easiest way to configure the library is to call <b>radius_config()</b>. <b>radius_config()</b> causes the library to read a configuration file whose format is described in radius.conf.
@@ -370,7 +370,7 @@ function radius_close(resource $radius_handle) { }
  * @see radius_add_server()
  * @since 1.1.0
  */
-function radius_config(resource $radius_handle, $file) { }
+function radius_config($radius_handle, $file) { }
 
 /**
  * A Radius request consists of a code specifying the kind of request, and zero or more attributes which provide additional information. To begin constructing a new request, call <b>radius_create_request()</b>.<br />
@@ -382,4 +382,4 @@ function radius_config(resource $radius_handle, $file) { }
  * @see radius_send_request()
  * @since 1.1.0
  */
-function radius_create_request(resource $radius_handle, $type) { }
+function radius_create_request($radius_handle, $type) { }
