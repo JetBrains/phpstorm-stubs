8e9c7784e96257e4d8329b8cad82ce8048f772c5
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2018-04-23 15:11:24 +0200

    Add list with muted problems

--- tests/TestStubs.php
+++ tests/TestStubs.php
@@ -32,8 +32,89 @@ class PhpStormStubsSingleton
     }
 }
 
+class MutedProblems
+{
+    /** @var stdClass */
+    private $mutedProblems;
+
+    public function __construct()
+    {
+        $json = file_get_contents(__DIR__ . "/mutedProblems.json");
+        $this->mutedProblems = json_decode($json);
+    }
+
+    public function getMutedProblemsForConstant(string $constantName): array
+    {
+        foreach ($this->mutedProblems->constants as $constant) {
+            if ($constant->name == $constantName) {
+                return $constant->problems;
+            }
+        }
+        return [];
+    }
+
+    public function getMutedProblemsForFunction(string $functionName): array
+    {
+        foreach ($this->mutedProblems->functions as $function) {
+            if ($function->name == $functionName) {
+                return $function->problems;
+            }
+        }
+        return [];
+    }
+
+    public function getMutedProblemsForClass(string $className): array
+    {
+        foreach ($this->mutedProblems->classes as $class) {
+            if ($class->name == $className) {
+                if(!empty($class->problems)) {
+                    return $class->problems;
+                }
+            }
+        }
+        return [];
+    }
+
+    public function getMutedProblemsForMethod(string $className, $methodName): array
+    {
+        foreach ($this->mutedProblems->classes as $class) {
+            if ($class->name == $className) {
+                foreach ($class->methods as $method) {
+                    if ($method->name == $methodName) {
+                        return $method->problems;
+                    }
+                }
+            }
+        }
+        return [];
+    }
+
+    public function getMutedProblemsForClassConstants($className, $constantName)
+    {
+        foreach ($this->mutedProblems->classes as $class) {
+            if ($class->name == $className) {
+                foreach ($class->constants as $constant) {
+                    if ($constant->name == $constantName) {
+                        return $constant->problems;
+                    }
+                }
+            }
+        }
+        return [];
+    }
+}
+
 class TestStubs extends TestCase
 {
+    /** @var MutedProblems */
+    private static $mutedProblems;
+
+    public static function setUpBeforeClass()/* The :void return type declaration that should be here would cause a BC issue */
+    {
+        self::$mutedProblems = new MutedProblems();
+    }
+
+
     public function constantProvider()
     {
         foreach (ReflectionStubsSingleton::getReflectionStubs()->constants as $constant) {
@@ -49,7 +130,9 @@ class TestStubs extends TestCase
         $constantName = $constant->name;
         $constantValue = $constant->value;
         $stubConstants = PhpStormStubsSingleton::getPhpStormStubs()->constants;
-        $this->assertArrayHasKey($constantName, $stubConstants, "Missing constant: const $constantName = $constantValue\n");
+        if (!in_array("missing constant", self::$mutedProblems->getMutedProblemsForConstant($constantName))) {
+            $this->assertArrayHasKey($constantName, $stubConstants, "Missing constant: const $constantName = $constantValue\n");
+        }
     }
 
 
@@ -68,11 +151,17 @@ class TestStubs extends TestCase
         $functionName = $function->name;
         $stubFunctions = PhpStormStubsSingleton::getPhpStormStubs()->functions;
         $params = $this->getParameterRepresentation($function);
-        $this->assertArrayHasKey($functionName, $stubFunctions, "Missing function: function $functionName($params){}");
-        $phpstormFunction = $stubFunctions[$functionName];
-        $this->assertFalse($function->is_deprecated && $phpstormFunction->is_deprecated != true, "Function $functionName is not deprecated in stubs");
-        $this->assertSameSize($function->parameters, $phpstormFunction->parameters,
-            "Parameter number mismatch for function $functionName. Expected: " . $this->getParameterRepresentation($function));
+        if (!in_array("missing function", self::$mutedProblems->getMutedProblemsForFunction($functionName))) {
+            $this->assertArrayHasKey($functionName, $stubFunctions, "Missing function: function $functionName($params){}");
+            $phpstormFunction = $stubFunctions[$functionName];
+            if (!in_array("deprecated function", self::$mutedProblems->getMutedProblemsForFunction($functionName))) {
+                $this->assertFalse($function->is_deprecated && $phpstormFunction->is_deprecated != true, "Function $functionName is not deprecated in stubs");
+            }
+            if (!in_array("parameter mismatch", self::$mutedProblems->getMutedProblemsForFunction($functionName))) {
+                $this->assertSameSize($function->parameters, $phpstormFunction->parameters,
+                    "Parameter number mismatch for function $functionName. Expected: " . $this->getParameterRepresentation($function));
+            }
+        }
     }
 
 
@@ -80,7 +169,7 @@ class TestStubs extends TestCase
     {
         foreach (ReflectionStubsSingleton::getReflectionStubs()->classes as $class) {
             //exclude classes from PHPReflectionParser
-            if(substr( $class->name, 0, 3 )!= "PHP") {
+            if (substr($class->name, 0, 3) != "PHP") {
                 yield "class {$class->name}" => [$class];
             }
         }
@@ -93,23 +182,39 @@ class TestStubs extends TestCase
     {
         $className = $class->name;
         $stubClasses = PhpStormStubsSingleton::getPhpStormStubs()->classes;
-        $this->assertArrayHasKey($className, $stubClasses, "Missing class $className: class $className {}");
-        $stubClass = $stubClasses[$className];
-        $this->assertEquals($class->parentClass, $stubClass->parentClass, "Class $className should extend {$class->parentClass}");
-        foreach ($class->constants as $constant) {
-            $this->assertArrayHasKey($constant->name, $stubClass->constants, "Missing constant $className::{$constant->name}");
-        }
-        // @todo check interfaces
-        // @todo check traits
-        foreach ($class->methods as $method) {
-            $params = $this->getParameterRepresentation($method);
-            $methodName = $method->name;
-            $this->assertArrayHasKey($methodName, $stubClass->methods, "Missing method $className::$methodName($params){}");
-            $stubMethod = $stubClass->methods[$methodName];
-            $this->assertEquals($method->is_final, $stubMethod->is_final, "Method $className::$methodName final modifier is incorrect");
-            $this->assertEquals($method->is_static, $stubMethod->is_static, "Method $className::$methodName static modifier is incorrect");
-            $this->assertEquals($method->access, $stubMethod->access, "Method $className::$methodName access modifier is incorrect");
-            $this->assertSameSize($method->parameters, $stubMethod->parameters, "Parameter number mismatch for method $className::$methodName. Expected: " . $this->getParameterRepresentation($method));
+        if (!in_array("missing class", self::$mutedProblems->getMutedProblemsForClass($className))) {
+            $this->assertArrayHasKey($className, $stubClasses, "Missing class $className: class $className {}");
+            $stubClass = $stubClasses[$className];
+            if (!in_array("wrong parent", self::$mutedProblems->getMutedProblemsForClass($className))) {
+                $this->assertEquals($class->parentClass, $stubClass->parentClass, "Class $className should extend {$class->parentClass}");
+            }
+            foreach ($class->constants as $constant) {
+                if (!in_array("missing constant", self::$mutedProblems->getMutedProblemsForClassConstants($className, $constant->name))) {
+                    $this->assertArrayHasKey($constant->name, $stubClass->constants, "Missing constant $className::{$constant->name}");
+                }
+            }
+            // @todo check interfaces
+            // @todo check traits
+            foreach ($class->methods as $method) {
+                $params = $this->getParameterRepresentation($method);
+                $methodName = $method->name;
+                if (!in_array("missing method", self::$mutedProblems->getMutedProblemsForMethod($className, $methodName))) {
+                    $this->assertArrayHasKey($methodName, $stubClass->methods, "Missing method $className::$methodName($params){}");
+                }
+                $stubMethod = $stubClass->methods[$methodName];
+                if (!in_array("not final", self::$mutedProblems->getMutedProblemsForMethod($className, $methodName))) {
+                    $this->assertEquals($method->is_final, $stubMethod->is_final, "Method $className::$methodName final modifier is incorrect");
+                }
+                if (!in_array("not static", self::$mutedProblems->getMutedProblemsForMethod($className, $methodName))) {
+                    $this->assertEquals($method->is_static, $stubMethod->is_static, "Method $className::$methodName static modifier is incorrect");
+                }
+                if (!in_array("access modifiers", self::$mutedProblems->getMutedProblemsForMethod($className, $methodName))) {
+                    $this->assertEquals($method->access, $stubMethod->access, "Method $className::$methodName access modifier is incorrect");
+                }
+                if (!in_array("parameter mismatch", self::$mutedProblems->getMutedProblemsForMethod($className, $methodName))) {
+                    $this->assertSameSize($method->parameters, $stubMethod->parameters, "Parameter number mismatch for method $className::$methodName. Expected: " . $this->getParameterRepresentation($method));
+                }
+            }
         }
     }
 
@@ -127,7 +232,7 @@ class TestStubs extends TestCase
             if ($parameter->is_vararg) {
                 $result .= "...";
             }
-            $result .= "$". $parameter->name . ", ";
+            $result .= "$" . $parameter->name . ", ";
         }
         $result = rtrim($result, ", ");
         return $result;
