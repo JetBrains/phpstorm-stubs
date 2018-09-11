3005c83834782d19747c1c9e1dccc3c4f11ee0fb
Marcin Pudełek <mpudelek@widzew.net>
2018-09-11 09:58:40 +0200

    MongoDB\BSON\JavascriptInterface

--- mongodb/mongodb.php
+++ mongodb/mongodb.php
@@ -2037,4 +2037,37 @@ namespace MongoDB {}
              */
             public function __toString();
         }
+
+        /**
+         * Interface JavascriptInterface
+         *
+         * @link https://secure.php.net/manual/en/class.mongodb-bson-javascriptinterface.php
+         * @since 1.3.0
+         */
+        interface JavascriptInterface
+        {
+            /**
+             * Returns the Javascript's code
+             * @return string
+             * @link https://secure.php.net/manual/en/mongodb-bson-javascriptinterface.getcode.php
+             * @since 1.3.0
+             */
+            public function getCode();
+
+            /**
+             * Returns the JavascriptInterface's scope document
+             * @return object|null
+             * @link https://secure.php.net/manual/en/mongodb-bson-javascriptinterface.getscope.php
+             * @since 1.3.0
+             */
+            public function getScope();
+
+            /**
+             * Returns the JavascriptInterface's code
+             * @return string
+             * @link https://secure.php.net/manual/en/mongodb-bson-javascriptinterface.tostring.php
+             * @since 1.3.0
+             */
+            public function __toString();
+        }
     }
