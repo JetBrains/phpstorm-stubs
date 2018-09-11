ce05ddaf8c73186efed1546b5882b60eee2b25e1
Marcin Pudełek <mpudelek@widzew.net>
2018-09-11 10:46:54 +0200

    MongoDB\Driver\Command::__construct

--- mongodb/mongodb.php
+++ mongodb/mongodb.php
@@ -418,15 +418,19 @@ namespace MongoDB {}
          * The MongoDB\Driver\Command class is a value object that represents a database command.
          * To provide "Command Helpers" the MongoDB\Driver\Command object should be composed.
          * @link https://php.net/manual/en/class.mongodb-driver-command.php
+         * @since 1.0.0
          */
         final class Command
         {
             /**
              * Construct new Command
              * @param array|object $document The complete command to construct
+             * @param array $commandOptions Do not use this parameter to specify options described in the command's reference in the MongoDB manual.
              * @throws InvalidArgumentException on argument parsing errors.
+             * @link https://secure.php.net/manual/en/mongodb-driver-command.construct.php
+             * @since 1.0.0
              */
-            final public function __construct($document)
+            final public function __construct($document, array $commandOptions = [])
             {
             }
         }
