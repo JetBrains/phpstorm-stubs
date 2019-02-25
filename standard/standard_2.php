e2eec4858ae271a2810adb18b738e560f950c103
Ben Mewburn <ben@xps.mewburn>
2019-02-26 10:40:22 +1100

    rand() param types and default args

--- standard/standard_2.php
+++ standard/standard_2.php
@@ -956,14 +956,14 @@ function proc_nice ($increment) {}
 /**
  * Generate a random integer
  * @link https://php.net/manual/en/function.rand.php
- * @param $min [optional]
- * @param $max [optional]
+ * @param int $min [optional]
+ * @param int $max [optional]
  * @return int A pseudo random value between min
  * (or 0) and max (or getrandmax, inclusive).
  * @since 4.0
  * @since 5.0
  */
-function rand ($min, $max) {}
+function rand ($min = 0, $max = null) {}
 
 /**
  * Seed the random number generator
