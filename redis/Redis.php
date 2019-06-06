b58d1b368f65c12d1666999abfe6bb0a877841f0
Alexander Schranz <alexander@sulu.io>
2019-06-06 17:51:59 +0200

    Add maxlength and approximate argument to xadd

--- redis/Redis.php
+++ redis/Redis.php
@@ -3341,6 +3341,8 @@ class Redis
      * @param   string  $str_key
      * @param   string  $str_id
      * @param   array   $arr_message
+     * @param   int     $i_maxlen
+     * @param   bool    $boo_approximate
      * @return  string  The added message ID.
      * @link    https://redis.io/commands/xadd
      * @example
@@ -3348,7 +3350,7 @@ class Redis
      * $obj_redis->xAdd('mystream', "*", ['field' => 'value']);
      * </pre>
      */
-    public function xAdd($str_key, $str_id, $arr_message) {}
+    public function xAdd($str_key, $str_id, $arr_message, $i_maxlen = 0, $boo_approximate = false) {}
 
     /**
      * Claim ownership of one or more pending messages.
