e6ff6c07d8b61fdce11b01de0fe44450407b937e
Patrik Foldes <pf@csgo.com>
2019-10-16 11:40:00 +0300

    Fixed docblock spacing, added correct version numbers

--- pcov/pcov.php
+++ pcov/pcov.php
@@ -13,56 +13,56 @@ namespace
 namespace pcov
 {
     /**
+     * (PHP &gt;= 7.0, PECL pcov &gt;= 1.0.0)<br/>
      * Shall start recording coverage information
      * @return void
-     * @since 7.0
      */
     function start () {}
 
     /**
+     * (PHP &gt;= 7.0, PECL pcov &gt;= 1.0.0)<br/>
      * Shall stop recording coverage information
      * @return void
-     * @since 7.0
      */
     function stop() {}
 
-    /*
-    * Shall collect coverage information
-    * @param int $type [optional] <p>
-    * pcov\all shall collect coverage information for all files
-    * pcov\inclusive shall collect coverage information for the specified files
-    * pcov\exclusive shall collect coverage information for all but the specified files
-    * </p>
-    * @param array $filenames [optional] <p>
-    * Note: paths in filter must be realpath
-    * </p>
-    * @return array
-    * @since 7.0
-    */
+    /**
+     * (PHP &gt;= 7.0, PECL pcov &gt;= 1.0.0)<br/>
+     * Shall collect coverage information
+     * @param int $type [optional] <p>
+     * pcov\all shall collect coverage information for all files
+     * pcov\inclusive shall collect coverage information for the specified files
+     * pcov\exclusive shall collect coverage information for all but the specified files
+     * </p>
+     * @param array $filenames [optional] <p>
+     * Note: paths in filter must be realpath
+     * </p>
+     * @return array
+     */
     function collect($type = all, $filter = []) {}
 
-    /*
-    * Shall clear stored information
-    * @param bool $files [optional] <p>
-    * set true to clear file tables
-    * Note: clearing the file tables may have surprising consequences
-    * </p>
-    * @return void
-    * @since 7.0
-    */
+    /**
+     * (PHP &gt;= 7.0, PECL pcov &gt;= 1.0.0)<br/>
+     * Shall clear stored information
+     * @param bool $files [optional] <p>
+     * set true to clear file tables
+     * Note: clearing the file tables may have surprising consequences
+     * </p>
+     * @return void
+     */
     function clear($files = false) {}
 
-    /*
-    * Shall return list of files waiting to be collected
-    * @return array
-    * @since 7.0
-    */
+    /**
+     * (PHP &gt;= 7.0, PECL pcov &gt;= 1.0.0)<br/>
+     * Shall return list of files waiting to be collected
+     * @return array
+     */
     function waiting() {}
 
-    /*
-    * Shall return the current size of the trace and cfg arena
-    * @return int
-    * @since 7.0
-    */
+    /**
+     * (PHP &gt;= 7.0, PECL pcov &gt;= 1.0.0)<br/>
+     * Shall return the current size of the trace and cfg arena
+     * @return int
+     */
     function memory() {}
 }
