4d6af95c7315b3e227c38f1f4965cc21220edd2d
KapitanOczywisty <44417092+KapitanOczywisty@users.noreply.github.com>
2019-12-13 13:55:49 +0100

    DOMDocument override return type

--- meta/.phpstorm.meta.php
+++ meta/.phpstorm.meta.php
@@ -83,6 +83,12 @@ namespace PHPSTORM_META {
 
   override(\str_replace(0), type(2));
 
+  override(\DOMDocument::importNode(0), type(0));
+  override(\DOMNode::appendChild(0), type(0));
+  override(\DOMNode::insertBefore(0), type(0));
+  override(\DOMNode::removeChild(0), type(0));
+  override(\DOMNode::replaceChild(0), type(1));
+
     function expectedArguments($functionReference, $argumentIndex, $values) {
         return "expectedArguments " . $functionReference . "at " . $argumentIndex . ": " . $values;
     }
