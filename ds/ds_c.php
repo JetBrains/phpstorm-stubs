4d3c2a738a44d0e1d0489875ef82ca4b0b60d032
Dominic Guhl <dguhl@users.noreply.github.com>
2020-02-22 14:38:23 +0100

    Added Deque DataStructure

--- ds/ds_c.php
+++ ds/ds_c.php
@@ -794,6 +794,400 @@ namespace Ds {
         }
 
         /**
+         * @inheritDoc
+         */
+        public function jsonSerialize()
+        {
+        }
+    }
+
+    class Deque implements Sequence
+    {
+        /**
+         * Count elements of an object
+         * @link https://php.net/manual/en/countable.count.php
+         * @return int The custom count as an integer.
+         * </p>
+         * <p>
+         * The return value is cast to an integer.
+         * @since 5.1
+         */
+        public function count(): int
+        {
+        }
+
+        /**
+         * Removes all values from the deque.
+         * @link https://www.php.net/manual/en/ds-deque.clear.php
+         */
+        public function clear(): void
+        {
+        }
+
+        /**
+         * Returns a shallow copy of the deque.
+         * @link https://www.php.net/manual/en/ds-deque.copy.php
+         * @return Collection
+         */
+        public function copy(): Collection
+        {
+        }
+
+        /**
+         * Returns whether the deque is empty.
+         * @link https://www.php.net/manual/en/ds-deque.isempty.php
+         * @return bool
+         */
+        public function isEmpty(): bool
+        {
+        }
+
+        /**
+         * Converts the deque to an array.
+         * <p>Note: Casting to an array is not supported yet.
+         * @link https://www.php.net/manual/en/ds-deque.toarray.php
+         * @return array An array containing all the values in the same order as
+         * the deque.
+         */
+        public function toArray(): array
+        {
+        }
+
+        /**
+         * Ensures that enough memory is allocated for a required capacity.
+         * This removes the need to reallocate the internal as values are added.
+         *
+         * @param int $capacity The number of values for which capacity should
+         * be allocated.<p>Note: Capacity will stay the same if this value is
+         * less than or equal to the current capacity.
+         * <p>Note: Capacity will always be rounded up to the nearest power of 2.
+         * @link https://www.php.net/manual/en/ds-deque.allocate.php
+         */
+        public function allocate(int $capacity): void
+        {
+        }
+
+        /**
+         * Updates all values by applying a callback function to each value in
+         * the deque.
+         * @param callable $callback A callable to apply to each value in the
+         * deque. The callback should return what the value should be
+         * replaced by.<p>
+         * <code>callback ( mixed $value ) : mixed</code>
+         * @link https://www.php.net/manual/en/ds-deque.apply.php
+         */
+        public function apply(callable $callback): void
+        {
+        }
+
+        /**
+         * Returns the current capacity.
+         * @return int The current capacity.
+         * @link https://www.php.net/manual/en/ds-deque.capacity.php
+         */
+        public function capacity(): int
+        {
+        }
+
+        /**
+         * Determines if the deque contains all values.
+         * @param mixed $values Values to check.
+         * @return bool FALSE if any of the provided values are not in the
+         * deque, TRUE otherwise.
+         * @link https://www.php.net/manual/en/ds-deque.contains.php
+         */
+        public function contains(...$values): bool
+        {
+        }
+
+        /**
+         * Creates a new deque using a callable to determine which values
+         * to include.
+         * @param callable $callback Optional callable which returns TRUE if the
+         * value should be included, FALSE otherwise. If a callback is not
+         * provided, only values which are TRUE (see converting to boolean) will
+         * be included.<p>
+         * <code>callback ( mixed $value ) : bool</code>
+         * @return Deque A new deque containing all the values for which
+         * either the callback returned TRUE, or all values that convert to
+         * TRUE if a callback was not provided.
+         * @link https://www.php.net/manual/en/ds-deque.filter.php
+         */
+        public function filter(callable $callback = null): Deque
+        {
+        }
+
+        /**
+         * Returns the index of the value, or FALSE if not found.
+         * @param mixed $value The value to find.
+         * @return int|bool The index of the value, or FALSE if not found.
+         * @link https://www.php.net/manual/en/ds-deque.find.php
+         */
+        public function find($value)
+        {
+        }
+
+        /**
+         * Returns the first value in the deque.
+         * @return mixed The first value in the deque.
+         * @throws UnderflowException if empty.
+         * @link https://www.php.net/manual/en/ds-deque.first.php
+         */
+        public function first()
+        {
+        }
+
+        /**
+         * Returns the value at a given index.
+         * @param int $index The index to access, starting at 0.
+         * @return mixed The value at the requested index.
+         * @throws OutOfRangeException if the index is not valid.
+         * @link https://www.php.net/manual/en/ds-deque.get.php
+         */
+        public function get(int $index)
+        {
+        }
+
+        /**
+         * Inserts values into the deque at a given index.
+         *
+         * @param int $index The index at which to insert. 0 <= index <= count
+         * <p> Note: You can insert at the index equal to the number of values.
+         * @param mixed ...$values The value or values to insert.
+         * @throws OutOfRangeException if the index is not valid.
+         * @link https://www.php.net/manual/en/ds-deque.insert.php
+         */
+        public function insert(int $index, ...$values): void
+        {
+        }
+
+        /**
+         * Joins all values together as a string using an optional separator
+         * between each value.
+         * @param string $glue An optional string to separate each value.
+         * @return string All values of the deque joined together as a
+         * string.
+         * @link https://www.php.net/manual/en/ds-deque.join.php
+         */
+        public function join(string $glue = ''): string
+        {
+        }
+
+        /**
+         * Returns the last value in the deque.
+         * @return mixed The last value in the deque.
+         * @throws UnderflowException if empty.
+         * @link https://www.php.net/manual/en/ds-deque.last.php
+         */
+        public function last()
+        {
+        }
+
+        /**
+         * Returns the result of applying a callback function to each value in
+         * the deque.
+         * @param callable $callback A callable to apply to each value in the
+         * deque.
+         * The callable should return what the new value will be in the new
+         * deque.
+         * <code>callback ( mixed $value ) : mixed</code>
+         * @return Deque The result of applying a callback to each value in
+         * the deque.<p>Note: The values of the current instance won't be
+         * affected.
+         * @link https://www.php.net/manual/en/ds-deque.map.php
+         */
+        public function map(callable $callback): Deque
+        {
+        }
+
+        /**
+         * Returns the result of adding all given values to the deque.
+         * @param mixed $values A traversable object or an array.
+         * @return Deque The result of adding all given values to the
+         * deque, effectively the same as adding the values to a copy,
+         * then returning that copy.
+         * @link https://www.php.net/manual/en/ds-deque.merge.php
+         */
+        public function merge($values): Deque
+        {
+        }
+
+        /**
+         * Removes and returns the last value.
+         * @return mixed The removed last value.
+         * @throws UnderflowException if empty.
+         * @link https://www.php.net/manual/en/ds-deque.pop.php
+         */
+        public function pop()
+        {
+        }
+
+        /**
+         * Adds values to the end of the deque.
+         * @param mixed ...$values The values to add.
+         */
+        public function push(...$values): void
+        {
+        }
+
+        /**
+         * Reduces the deque to a single value using a callback function.
+         * @param callable $callback <p><p>
+         * <code>
+         * callback ( mixed $carry , mixed $value ) : mixed</code>
+         * <b>$carry</b> The return value of the previous callback, or initial if it's
+         * the first iteration.<p>
+         * <b>$value</b> The value of the current iteration.
+         * @param mixed $initial The initial value of the carry value. Can be NULL.
+         * @return mixed The return value of the final callback.
+         * @link https://www.php.net/manual/en/ds-deque.reduce.php
+         */
+        public function reduce(callable $callback, $initial = null)
+        {
+        }
+
+        /**
+         * Removes and returns a value by index.
+         * @param int $index The index of the value to remove.
+         * @return mixed The value that was removed.
+         * @link https://www.php.net/manual/en/ds-deque.remove.php
+         */
+        public function remove(int $index)
+        {
+        }
+
+        /**
+         * Reverses the deque in-place.
+         * @link https://www.php.net/manual/en/ds-deque.reverse.php
+         */
+        public function reverse(): void
+        {
+        }
+
+        /**
+         * Returns a reversed copy of the deque.
+         * @return Deque A reversed copy of the deque.
+         * <p>Note: The current instance is not affected.
+         */
+        public function reversed(): Deque
+        {
+        }
+
+        /**
+         * Rotates the deque by a given number of rotations, which is
+         * equivalent to successively calling
+         * $deque->push($deque->shift()) if the number of rotations is
+         * positive, or $deque->unshift($deque->pop()) if negative.
+         * @param int $rotations The number of times the deque should be
+         * rotated.
+         * @link https://www.php.net/manual/en/ds-deque.rotate.php
+         */
+        public function rotate(int $rotations): void
+        {
+        }
+
+        /**
+         * Updates a value at a given index.
+         * @param int $index The index of the value to update.
+         * @param mixed $value The new value.
+         * @throws OutOfRangeException if the index is not valid.
+         * @link https://www.php.net/manual/en/ds-deque.set.php
+         */
+        public function set(int $index, $value): void
+        {
+        }
+
+        /**
+         * Removes and returns the first value.
+         * @return mixed
+         * @throws UnderflowException if empty.
+         * @link https://www.php.net/manual/en/ds-deque.shift.php
+         */
+        public function shift()
+        {
+        }
+
+        /**
+         * Creates a sub-deque of a given range.
+         * @param int $index The index at which the sub-deque starts.
+         * If positive, the deque will start at that index in the deque.
+         * If negative, the deque will start that far from the end.
+         * @param int|null $length If a length is given and is positive, the
+         * resulting deque will have up to that many values in it. If the
+         * length results in an overflow, only values up to the end of the
+         * deque will be included. If a length is given and is negative,
+         * the deque will stop that many values from the end. If a length
+         * is not provided, the resulting deque will contain all values
+         * between the index and the end of the deque.
+         * @return Deque A sub-deque of the given range.
+         * @link https://www.php.net/manual/en/ds-deque.slice.php
+         */
+        public function slice(int $index, int $length = null): Deque
+        {
+        }
+
+        /**
+         * Sorts the deque in-place, using an optional comparator function.
+         * @param callable|null $comparator The comparison function must return
+         * an integer less than, equal to, or greater than zero if the first
+         * argument is considered to be respectively less than, equal to, or
+         * greater than the second. Note that before PHP 7.0.0 this integer had
+         * to be in the range from -2147483648 to 2147483647.<p>
+         * <code>callback ( mixed $a, mixed $b ) : int</code>
+         * <p>Caution: Returning non-integer values from the comparison
+         * function, such as float, will result in an internal cast to integer
+         * of the callback's return value. So values such as 0.99 and 0.1 will
+         * both be cast to an integer value of 0, which will compare such
+         * values as equal.
+         * @link https://www.php.net/manual/en/ds-deque.sort.php
+         */
+        public function sort(callable $comparator = null): void
+        {
+        }
+
+        /**
+         * Returns a sorted copy, using an optional comparator function.
+         * @param callable|null $comparator The comparison function must return
+         * an integer less than, equal to, or greater than zero if the first
+         * argument is considered to be respectively less than, equal to, or
+         * greater than the second. Note that before PHP 7.0.0 this integer had
+         * to be in the range from -2147483648 to 2147483647.<p>
+         * <code>callback ( mixed $a, mixed $b ) : int</code>
+         * <p>Caution: Returning non-integer values from the comparison
+         * function, such as float, will result in an internal cast to integer
+         * of the callback's return value. So values such as 0.99 and 0.1 will
+         * both be cast to an integer value of 0, which will compare such
+         * values as equal.
+         * @return Deque Returns a sorted copy of the deque.
+         * @link https://www.php.net/manual/en/ds-deque.sort.php
+         */
+        public function sorted(callable $comparator): Deque
+        {
+        }
+
+        /**
+         * Returns the sum of all values in the deque.
+         * <p>Note: Arrays and objects are considered equal to zero when
+         * calculating the sum.
+         * @return float|int The sum of all the values in the deque as
+         * either a float or int depending on the values in the deque.
+         */
+        public function sum(): float
+        {
+        }
+
+        /**
+         * Adds values to the front of the deque, moving all the current
+         * values forward to make room for the new values.
+         * @param mixed $values The values to add to the front of the deque.
+         * <p>Note: Multiple values will be added in the same order that they
+         * are passed.
+         */
+        public function unshift($values): void
+        {
+        }
+
+        /**
          * Specify data which should be serialized to JSON
          * @link https://php.net/manual/en/jsonserializable.jsonserialize.php
          * @return mixed data which can be serialized by <b>json_encode</b>,
@@ -803,5 +1197,7 @@ namespace Ds {
         public function jsonSerialize()
         {
         }
+
+
     }
 }
