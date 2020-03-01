0b02d29c5d9b05480ad4496990876d05d6867a4b
Kamil Tekiela <tekiela246@gmail.com>
2020-03-01 21:23:40 +0000

    Adding optional parameter to mysqli::store_result

--- mysqli/mysqli.php
+++ mysqli/mysqli.php
@@ -724,7 +724,7 @@ class mysqli  {
 	 * <b>mysqli_field_count</b> returns a non-zero value, the
 	 * statement should have produced a non-empty result set.
 	 */
-	public function store_result ($option) {}
+	public function store_result (int $option = null) {}
 
 	/**
 	 * Returns whether thread safety is given or not
@@ -2443,7 +2443,7 @@ function mysqli_stmt_store_result ($stmt) {}
  * @param int $option [optional] The option that you want to set
  * @return mysqli_result|false
  */
-function mysqli_store_result ($link, int $option) {}
+function mysqli_store_result ($link, int $option = null) {}
 
 /**
  * Returns the thread ID for the current connection
