e7fa6531bcf058fa458da795e7acbc2148b607e0
Serafim <nesk@xakep.ru>
2020-06-29 23:56:03 +0300

    [PHP 8.0] Added a WeakMap class

--- Core/Core_c.php
+++ Core/Core_c.php
@@ -662,6 +662,74 @@ class WeakReference {
 }
 
 /**
+ * Weak maps allow creating a map from objects to arbitrary values
+ * (similar to SplObjectStorage) without preventing the objects that are used
+ * as keys from being garbage collected. If an object key is garbage collected,
+ * it will simply be removed from the map.
+ *
+ * @since 8.0
+ */
+final class WeakMap implements \ArrayAccess, \Countable, \IteratorAggregate {
+    /**
+     * Returns {@see true} if the value for the object is contained in
+     * the {@see WeakMap} and {@see false} instead.
+     *
+     * @param object $object Any object
+     * @return bool
+     */
+    public function offsetExists($object): bool {}
+
+    /**
+     * Returns the existsing value by an object.
+     *
+     * @param object $object Any object
+     * @return mixed Value associated with the key object
+     */
+    public function offsetGet($object)
+    {
+    }
+
+    /**
+     * Sets a new value for an object.
+     *
+     * @param object $object Any object
+     * @param mixed $value Any value
+     * @return void
+     */
+    public function offsetSet($object, $value): void
+    {
+    }
+
+    /**
+     * Force removes an object value from the {@see WeakMap} instance.
+     *
+     * @param object $object Any object
+     * @return void
+     */
+    public function offsetUnset($object): void
+    {
+    }
+
+    /**
+     * Returns an iterator in the "[object => mixed]" format.
+     *
+     * @return Traversable
+     */
+    public function getIterator(): \Traversable
+    {
+    }
+
+    /**
+     * Returns the number of items in the {@see WeakMap} instance.
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+    }
+}
+
+    /**
  * Stringable interface marks classes as available for serialization
  * in a string.
  *
