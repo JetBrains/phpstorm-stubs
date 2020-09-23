4d77d358d9e7c1d0871d888f18f60239431612bc
Kirill Smelov <kirill.smelov@jetbrains.com>
2020-09-23 14:15:04 +0300

    WI-55548 Add type hints to ExpectedValues constructor

--- meta/attributes/ExpectedValues.php
+++ meta/attributes/ExpectedValues.php
@@ -35,7 +35,7 @@ use Attribute;
  */
 #[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_PARAMETER)]
 class ExpectedValues {
-    public function __construct($values = [], $flags = [], $valuesFromClass = null, $flagsFromClass = null)
+    public function __construct(array $values = [], array $flags = [], string $valuesFromClass = null, string $flagsFromClass = null)
     {
     }
 }
