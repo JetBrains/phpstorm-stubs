283077af53ba9319567ff1dd0caf15c5468209a0
Anton Monakov <anton.monakov@jetbrains.com>
2020-10-05 11:19:35 +0300

    Proofread ArrayShape description

--- meta/attributes/ArrayShape.php
+++ meta/attributes/ArrayShape.php
@@ -7,12 +7,13 @@ use Attribute;
 /**
  * The attribute specifies possible array keys and their types.
  *
- * If applied, IDE will suggest specified array keys, will infer specified types and will highlight non-specified keys in array access
+ * If applied, an IDE will suggest the specified array keys, infer the specified types, and highlight non-specified keys in array access expressions.
  *
- * Array shapes should be specified with required $shape parameter, it's values should be array literal.<br />
- * Here is sample usage: <br />
+ * Array shapes should be specified with the required $shape parameter whose values should be array literals.<br />
+ *
+ * Example: <br />
  * <b>#[ArrayShape(["f" => "int", "string", "x" => "float"])]</b>
- * This usage applied on element effectively means that array has dimension 3, keys are "f", 1, "x", corresponding types are "int", "string" and "float".
+ * This usage applied on an element effectively means that the array has 3 dimensions, the keys are "f", 1, and "x", and the corresponding types are "int", "string", and "float".
  */
 #[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
 class ArrayShape {
