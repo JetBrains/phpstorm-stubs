38e7f4fc5e25ce7df0080eceb08bce2500128525
Kirill Smelov <kirill.smelov@jetbrains.com>
2020-10-20 17:36:18 +0300

    #[Pure]: Core.php

--- Core/Core.php
+++ Core/Core.php
@@ -2,12 +2,14 @@
 
 // Start of Core v.5.3.6-13ubuntu3.2
 use JetBrains\PhpStorm\ArrayShape;
+use JetBrains\PhpStorm\Pure;
 
 /**
  * Gets the version of the current Zend engine
  * @link https://php.net/manual/en/function.zend-version.php
  * @return string the Zend Engine version number, as a string.
  */
+#[Pure]
 function zend_version () {}
 
 /**
@@ -16,6 +18,7 @@ function zend_version () {}
  * @return int the number of arguments passed into the current user-defined
  * function.
  */
+#[Pure]
 function func_num_args () {}
 
 /**
@@ -27,6 +30,7 @@ function func_num_args () {}
  * </p>
  * @return mixed|false the specified argument, or false on error.
  */
+#[Pure]
 function func_get_arg ($position) {}
 
 /**
@@ -35,6 +39,7 @@ function func_get_arg ($position) {}
  * @return array an array in which each element is a copy of the corresponding
  * member of the current user-defined function's argument list.
  */
+#[Pure]
 function func_get_args () {}
 
 /**
@@ -46,6 +51,7 @@ function func_get_args () {}
  * @return int The length of the <i>string</i> on success,
  * and 0 if the <i>string</i> is empty.
  */
+#[Pure]
 function strlen ($string) {}
 
 /**
@@ -62,6 +68,7 @@ function strlen ($string) {}
  * is greater than <i>str2</i>, and 0 if they are
  * equal.
  */
+#[Pure]
 function strcmp ($string1, $string2) {}
 
 /**
@@ -81,6 +88,7 @@ function strcmp ($string1, $string2) {}
  * is greater than <i>str2</i>, and 0 if they are
  * equal.
  */
+#[Pure]
 function strncmp ($string1, $string2, $length) {}
 
 /**
@@ -97,6 +105,7 @@ function strncmp ($string1, $string2, $length) {}
  * is greater than <i>str2</i>, and 0 if they are
  * equal.
  */
+#[Pure]
 function strcasecmp ($string1, $string2) {}
 
 /**
@@ -115,6 +124,7 @@ function strcasecmp ($string1, $string2) {}
  * <i>str2</i>; &gt; 0 if <i>str1</i> is
  * greater than <i>str2</i>, and 0 if they are equal.
  */
+#[Pure]
 function strncasecmp ($string1, $string2, $length) {}
 
 /**
@@ -126,6 +136,7 @@ function strncasecmp ($string1, $string2, $length) {}
  * @return bool
  * @since 8.0
  */
+#[Pure]
 function str_starts_with(string $haystack, string $needle) : bool {}
 
 /**
@@ -137,6 +148,7 @@ function str_starts_with(string $haystack, string $needle) : bool {}
  * @return bool
  * @since 8.0
  */
+#[Pure]
 function str_ends_with(string $haystack, string $needle) : bool {}
 
 /**
@@ -148,6 +160,7 @@ function str_ends_with(string $haystack, string $needle) : bool {}
  * @return bool
  * @since 8.0
  */
+#[Pure]
 function str_contains(string $haystack, string $needle) : bool {}
 
 /**
@@ -333,6 +346,7 @@ function define ($constant_name, $value, $case_insensitive = false) {}
  * @return bool true if the named constant given by <i>name</i>
  * has been defined, false otherwise.
  */
+#[Pure]
 function defined ($constant_name) {}
 
 /**
@@ -349,6 +363,7 @@ function defined ($constant_name) {}
  * If <i>object</i> is omitted when inside a class, the
  * name of that class is returned.
  */
+#[Pure]
 function get_class ($object = null) {}
 
 /**
@@ -356,6 +371,7 @@ function get_class ($object = null) {}
  * @link https://php.net/manual/en/function.get-called-class.php
  * @return string|false The class name. Returns false if called from outside a class.
  */
+#[Pure]
 function get_called_class () {}
 
 /**
@@ -373,6 +389,7 @@ function get_called_class () {}
  * <p>
  * If called without parameter outside object, this function returns false.
  */
+#[Pure]
 function get_parent_class ($object_or_class = null) {}
 
 /**
@@ -388,6 +405,7 @@ function get_parent_class ($object_or_class = null) {}
  * has been defined for the given <i>object</i>, false
  * otherwise.
  */
+#[Pure]
 function method_exists ($object_or_class, $method) {}
 
 /**
@@ -402,6 +420,7 @@ function method_exists ($object_or_class, $method) {}
  * @return bool true if the property exists, false if it doesn't exist or
  * null in case of an error.
  */
+#[Pure]
 function property_exists ($object_or_class, $property) {}
 
 /**
@@ -412,6 +431,7 @@ function property_exists ($object_or_class, $property) {}
  * @link https://secure.php.net/manual/en/function.trait-exists.php
  * @since 5.4
  */
+#[Pure]
 function trait_exists($trait, $autoload ) {}
 
 /**
@@ -426,6 +446,7 @@ function trait_exists($trait, $autoload ) {}
  * @return bool true if <i>class_name</i> is a defined class,
  * false otherwise.
  */
+#[Pure]
 function class_exists ($class, $autoload = true) {}
 
 /**
@@ -441,6 +462,7 @@ function class_exists ($class, $autoload = true) {}
  * <i>interface_name</i> has been defined, false otherwise.
  * @since 5.0.2
  */
+#[Pure]
 function interface_exists ($interface, $autoload = true) {}
 
 /**
@@ -456,6 +478,7 @@ function interface_exists ($interface, $autoload = true) {}
  * This function will return false for constructs, such as
  * <b>include_once</b> and <b>echo</b>.
  */
+#[Pure]
 function function_exists ($function) {}
 
 /**
@@ -482,6 +505,7 @@ function class_alias ($class, $alias, $autoload = true) {}
  * Files that are included or required multiple times only show up once in
  * the returned array.
  */
+#[Pure]
 function get_included_files () {}
 
 /**
@@ -489,6 +513,7 @@ function get_included_files () {}
  * @link https://php.net/manual/en/function.get-required-files.php
  * @return string[]
  */
+#[Pure]
 function get_required_files () {}
 
 /**
@@ -508,6 +533,7 @@ function get_required_files () {}
  * belongs to a class which is a subclass of
  * <i>class_name</i>, false otherwise.
  */
+#[Pure]
 function is_subclass_of ($object_or_class, $class, $allow_string = true) {}
 
 /**
@@ -526,6 +552,7 @@ function is_subclass_of ($object_or_class, $class, $allow_string = true) {}
  * @return bool <b>TRUE</b> if the object is of this class or has this class as one of
  * its parents, <b>FALSE</b> otherwise.
  */
+#[Pure]
 function is_a ($object_or_class, $class, $allow_string = false) {}
 
 /**
@@ -539,6 +566,7 @@ function is_a ($object_or_class, $class, $allow_string = false) {}
  * The resulting array elements are in the form of
  * varname => value.
  */
+#[Pure]
 function get_class_vars ($class) {}
 
 /**
@@ -551,6 +579,7 @@ function get_class_vars ($class) {}
  * for the specified <i>object</i> in scope. If a property have
  * not been assigned a value, it will be returned with a null value.
  */
+#[Pure]
 function get_object_vars ($object) {}
 
 /**
@@ -562,6 +591,7 @@ function get_object_vars ($object) {}
  * @return array an array of method names defined for the class specified by
  * <i>class_name</i>. In case of an error, it returns null.
  */
+#[Pure]
 function get_class_methods ($object_or_class) {}
 
 /**
@@ -670,6 +700,7 @@ function restore_exception_handler () {}
  * names. There is a list of predefined classes in the Predefined Classes section of
  * the appendices.
  */
+#[Pure]
 function get_declared_classes () {}
 
 /**
@@ -678,6 +709,7 @@ function get_declared_classes () {}
  * @return array an array of the names of the declared interfaces in the current
  * script.
  */
+#[Pure]
 function get_declared_interfaces () {}
 
 /**
@@ -687,6 +719,7 @@ function get_declared_interfaces () {}
  * @see class_uses()
  * @since 5.4
  */
+#[Pure]
 function get_declared_traits() {}
 
 /**
@@ -699,6 +732,7 @@ function get_declared_traits() {}
  * the user defined ones using $arr["user"] (see example
  * below).
  */
+#[Pure]
 function get_defined_functions ($exclude_disabled = false) {}
 
 /**
@@ -706,6 +740,7 @@ function get_defined_functions ($exclude_disabled = false) {}
  * @link https://php.net/manual/en/function.get-defined-vars.php
  * @return array A multidimensional array with all the variables.
  */
+#[Pure]
 function get_defined_vars () {}
 
 /**
@@ -749,6 +784,7 @@ function get_resource_type ($resource) {}
  * </p>
  * @return array an indexed array of all the modules names.
  */
+#[Pure]
 function get_loaded_extensions ($zend_extensions = false) {}
 
 /**
@@ -783,6 +819,7 @@ function get_loaded_extensions ($zend_extensions = false) {}
  * @return bool true if the extension identified by <i>name</i>
  * is loaded, false otherwise.
  */
+#[Pure]
 function extension_loaded ($extension) {}
 
 /**
@@ -797,6 +834,7 @@ function extension_loaded ($extension) {}
  * @return string[]|false an array with all the functions, or false if
  * <i>module_name</i> is not a valid extension.
  */
+#[Pure]
 function get_extension_funcs ($extension) {}
 
 /**
@@ -853,6 +891,7 @@ function get_extension_funcs ($extension) {}
  * </p>
  * @return array
  */
+#[Pure]
 function get_defined_constants ($categorize = false) {}
 
 /**
@@ -989,6 +1028,7 @@ function gc_collect_cycles () {}
  * @link https://php.net/manual/en/function.gc-enabled.php
  * @return bool true if the garbage collector is enabled, false otherwise.
  */
+#[Pure]
 function gc_enabled () {}
 
 /**
@@ -1018,6 +1058,7 @@ function gc_disable () {}
  * @since 7.3
  */
 #[ArrayShape(["runs" => "int", "collected" => "int", "threshold" => "int", "roots" => "int"])]
+#[Pure]
 function gc_status () {}
 
 /**
@@ -1026,6 +1067,7 @@ function gc_status () {}
  * @return int Returns the number of bytes freed.
  * @since 7.0
  */
+#[Pure]
 function gc_mem_caches () {}
 
 /**
@@ -1042,4 +1084,5 @@ function gc_mem_caches () {}
  * @return array Returns an array of currently active resources, indexed by resource number.
  * @since 7.0
  */
+#[Pure]
 function get_resources ($type = null) {}
