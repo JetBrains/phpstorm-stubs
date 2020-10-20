cf1111719a8df2dc46651d170a9fb8673fc97d75
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2020-10-20 20:26:33 +0200

    Annotate bz2 functions with #[Pure]

--- bz2/bz2.php
+++ bz2/bz2.php
@@ -1,5 +1,7 @@
 <?php
 
+use JetBrains\PhpStorm\Pure;
+
 /**
  * Opens a bzip2 compressed file
  * @link https://php.net/manual/en/function.bzopen.php
@@ -14,6 +16,7 @@
  * @return resource|false If the open fails, <b>bzopen</b> returns <b>FALSE</b>, otherwise
  * it returns a pointer to the newly opened file.
  */
+#[Pure]
 function bzopen ($file, $mode) {}
 
 /**
@@ -30,6 +33,7 @@ function bzopen ($file, $mode) {}
  * </p>
  * @return string the uncompressed data, or <b>FALSE</b> on error.
  */
+#[Pure]
 function bzread ($bz, $length = 1024) {}
 
 /**
@@ -82,6 +86,7 @@ function bzclose ($bz) {}
  * </p>
  * @return int the error number as an integer.
  */
+#[Pure]
 function bzerrno ($bz) {}
 
 /**
@@ -93,6 +98,7 @@ function bzerrno ($bz) {}
  * </p>
  * @return string a string containing the error message.
  */
+#[Pure]
 function bzerrstr ($bz) {}
 
 /**
@@ -106,6 +112,7 @@ function bzerrstr ($bz) {}
  * errno entry, and the error message in the
  * errstr entry.
  */
+#[Pure]
 function bzerror ($bz) {}
 
 /**
@@ -130,6 +137,7 @@ function bzerror ($bz) {}
  * </p>
  * @return mixed The compressed string, or an error number if an error occurred.
  */
+#[Pure]
 function bzcompress ($data, $block_size = 4, $work_factor = 0) {}
 
 /**
@@ -149,4 +157,5 @@ function bzcompress ($data, $block_size = 4, $work_factor = 0) {}
  * </p>
  * @return mixed The decompressed string, or an error number if an error occurred.
  */
+#[Pure]
 function bzdecompress ($data, $use_less_memory = 0) {}
