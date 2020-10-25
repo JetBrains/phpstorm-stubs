cc8a312cb315e3d13be41e208d7889ca2278bf61
Anton <antonfedonjuk@gmail.com>
2020-10-25 16:37:41 +0300

    Update Reflector.php
    rollback, my translation fail

--- Reflection/Reflector.php
+++ Reflection/Reflector.php
@@ -21,6 +21,9 @@ interface Reflector extends Stringable
     /**
      * Returns the string representation of any Reflection object.
      *
+     * Please note that since PHP 8.0 this method is absent in this interface
+     * and inherits from the {@see Stringable} parent.
+     *
      * @return string
      */
     public function __toString();
