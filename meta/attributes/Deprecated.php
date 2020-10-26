22bc5296e39b6156677bfa6a455d13447ba1bc84
Kirill Smelov <kirill.smelov@jetbrains.com>
2020-10-26 17:23:14 +0300

    WI-56586 #[Deprecated]: support fields

--- meta/attributes/Deprecated.php
+++ meta/attributes/Deprecated.php
@@ -5,7 +5,7 @@ namespace JetBrains\PhpStorm;
 use Attribute;
 
 
-#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT)]
+#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_CLASS | Attribute::TARGET_CLASS_CONSTANT | Attribute::TARGET_PROPERTY)]
 class Deprecated {
     const PHP_VERSIONS = [
         "5.3",
