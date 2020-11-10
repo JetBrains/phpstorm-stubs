7b0e58faa9f714c5dbba4d963617f2695ebfc17a
Anton Monakov <anton.monakov@jetbrains.com>
2020-11-10 20:17:05 +0300

    Proofread 'Immutable' description

--- meta/attributes/Immutable.php
+++ meta/attributes/Immutable.php
@@ -5,14 +5,14 @@ namespace JetBrains\PhpStorm;
 use Attribute;
 
 /**
- * Mark property (or all properties of the class in case of class) as immutable.
- * IDE will highlight write accesses on such fields if they located outside constructor by default (this scope is customizable, see below).
+ * Mark a property (or all class properties in the case of a class) as immutable.
+ * By default, an IDE highlights write accesses on such properties if they are located outside a constructor (this scope is customizable, see below).
  *
- * One can provide custom allowed write scopes, possible values:
+ * You can provide a custom allowed write scope by using the following values:
  * <ul>
  * <li>{@link Immutable::CONSTRUCTOR_WRITE_SCOPE}: write is allowed only in containing class constructor (default choice)</li>
- * <li>{@link Immutable::PRIVATE_WRITE_SCOPE}: write is allowed only in places where field would be accessible if it would have 'private' visibility modifier</li>
- * <li>{@link Immutable::PROTECTED_WRITE_SCOPE}: write is allowed only in places where field would be accessible if it would have 'protected' visibility modifier</li>
+ * <li>{@link Immutable::PRIVATE_WRITE_SCOPE}: write is allowed only in places where the property would be accessible if it had 'private' visibility modifier</li>
+ * <li>{@link Immutable::PROTECTED_WRITE_SCOPE}: write is allowed only in places where the property would be accessible if it had 'protected' visibility modifier</li>
  * </ul>
  * @since 8.0
  */
