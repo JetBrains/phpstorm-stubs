685a72d91875101227619453816ac90ad41196a6
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2020-11-16 09:40:09 +0100

    Don't expand mixed type, it's too verbose

--- standard/standard_4.php
+++ standard/standard_4.php
@@ -171,7 +171,7 @@ function serialize ($value) {}
  * Omitting this option is the same as defining it as TRUE: PHP will attempt
  * to instantiate objects of any class.
  * </p>
- * @return bool|int|float|string|array|object The converted value is returned, and can be a boolean,
+ * @return mixed The converted value is returned, and can be a boolean,
  * integer, float, string,
  * array or object.
  * </p>
