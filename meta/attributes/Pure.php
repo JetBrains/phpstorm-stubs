11e125b55e756c7f0493172f2a330423e13740a2
Kirill Smelov <kirill.smelov@jetbrains.com>
2021-03-29 14:47:00 +0300

    WI-59474 Introduce Pure#mayDependOnGlobalScope

--- meta/attributes/Pure.php
+++ meta/attributes/Pure.php
@@ -10,5 +10,12 @@ use Attribute;
  *
  * @since 8.0
  */
-#[Attribute(Attribute::TARGET_FUNCTION|Attribute::TARGET_METHOD)]
-class Pure {}
+#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
+class Pure {
+    /**
+     * @param bool $mayDependOnGlobalScope Whether the function result may be dependendent on anything except passed variables
+     */
+    public function __construct(bool $mayDependOnGlobalScope = false)
+    {
+    }
+}
