41fddebdf853fc01b1d3a7b77f92b2976978086d
Blenderpony <60667840+Blenderpony@users.noreply.github.com>
2021-04-05 13:48:13 +0200

    Update CSPRNG.php
    Proposing changes based upon this ticket:
    https://youtrack.jetbrains.com/issue/WI-59611

--- standard/CSPRNG.php
+++ standard/CSPRNG.php
@@ -5,7 +5,7 @@
  * @param int $length The length of the random string that should be returned in bytes.
  * @return string Returns a string containing the requested number of cryptographically secure random bytes.
  * @since 7.0
- * @throws Exception if it was not possible to gather sufficient entropy.
+ * @throws Exception if an appropriate source of randomness cannot be found.
  */
 function random_bytes(int $length): string {}
 
@@ -16,6 +16,6 @@ function random_bytes(int $length): string {}
  * @param int $max The highest value to be returned, which must be less than or equal to PHP_INT_MAX.
  * @return int Returns a cryptographically secure random integer in the range min to max, inclusive.
  * @since 7.0
- * @throws Exception if it was not possible to gather sufficient entropy.
+ * @throws Exception if an appropriate source of randomness cannot be found.
  */
 function random_int(int $min, int $max): int {}
