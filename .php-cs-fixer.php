285efcf3455ff30e9da5a0cb4d88c240a15bf0d5
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-05-10 18:49:40 +0300

    add require to cs-fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 use StubTests\CodeStyle\BracesOneLineFixer;
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
-    ->append(['.php_cs'])
+    ->append(['.php-cs-fixer.php'])
     ->notName('PhpStormStubsMap.php');
 
 return (new PhpCsFixer\Config())
