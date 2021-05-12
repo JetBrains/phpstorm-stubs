ddd664ccdcb9d112a4e85db8c8c085981d417949
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-05-12 17:27:41 +0300

    WI-59707 add reflection classes for enum

---
+++ Reflection/ReflectionEnumBackedCase.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @link https://php.net/manual/en/class.reflectionenumbackedcase.php
+ * @since 8.1
+ */
+class ReflectionEnumBackedCase extends ReflectionEnumUnitCase
+{
+    public function getBackingValue(): int|string {}
+}
