89e1627acc4b0cb366c909b0dfb4a546fcd5e560
SerafimArts <nesk@xakep.ru>
2021-07-22 18:59:56 +0300

    Add new PHP 8.1 api support

--- FFI/.phpstorm.meta.php
+++ FFI/.phpstorm.meta.php
@@ -2,6 +2,7 @@
 
 namespace PHPSTORM_META {
 
+
     registerArgumentsSet('FFICType',
         'void *',
 
@@ -45,9 +46,68 @@ namespace PHPSTORM_META {
         'uint64_t',
     );
 
+    registerArgumentsSet('FFICTypeKind',
+        \FFI\CType::TYPE_VOID,
+        \FFI\CType::TYPE_FLOAT,
+        \FFI\CType::TYPE_DOUBLE,
+        \FFI\CType::TYPE_LONG_DOUBLE,
+        \FFI\CType::TYPE_UINT8,
+        \FFI\CType::TYPE_SINT8,
+        \FFI\CType::TYPE_UINT16,
+        \FFI\CType::TYPE_SINT16,
+        \FFI\CType::TYPE_UINT32,
+        \FFI\CType::TYPE_SINT32,
+        \FFI\CType::TYPE_UINT64,
+        \FFI\CType::TYPE_SINT64,
+        \FFI\CType::TYPE_ENUM,
+        \FFI\CType::TYPE_BOOL,
+        \FFI\CType::TYPE_CHAR,
+        \FFI\CType::TYPE_POINTER,
+        \FFI\CType::TYPE_FUNC,
+        \FFI\CType::TYPE_ARRAY,
+        \FFI\CType::TYPE_STRUCT
+    );
+
+    registerArgumentsSet('FFICTypeEnumKind',
+        \FFI\CType::TYPE_UINT32,
+        \FFI\CType::TYPE_UINT64,
+    );
+
+    registerArgumentsSet('FFICTypeAttr',
+        \FFI\CType::ATTR_CONST,
+        \FFI\CType::ATTR_INCOMPLETE_TAG,
+        \FFI\CType::ATTR_VARIADIC,
+        \FFI\CType::ATTR_INCOMPLETE_ARRAY,
+        \FFI\CType::ATTR_VLA,
+        \FFI\CType::ATTR_UNION,
+        \FFI\CType::ATTR_PACKED,
+        \FFI\CType::ATTR_MS_STRUCT,
+        \FFI\CType::ATTR_GCC_STRUCT,
+    );
+
+    registerArgumentsSet('FFICTypeAbi',
+        \FFI\CType::ABI_DEFAULT,
+        \FFI\CType::ABI_CDECL,
+        \FFI\CType::ABI_FASTCALL,
+        \FFI\CType::ABI_THISCALL,
+        \FFI\CType::ABI_STDCALL,
+        \FFI\CType::ABI_PASCAL,
+        \FFI\CType::ABI_REGISTER,
+        \FFI\CType::ABI_MS,
+        \FFI\CType::ABI_SYSV,
+        \FFI\CType::ABI_VECTORCALL
+    );
+
     expectedArguments(\FFI::new(), 0, argumentsSet('FFICType'));
     expectedArguments(\FFI::cast(), 0, argumentsSet('FFICType'));
     expectedArguments(\FFI::type(), 0, argumentsSet('FFICType'));
+
+    expectedReturnValues(\FFI\CType::getKind(), argumentsSet('FFICTypeKind'));
+    expectedReturnValues(\FFI\CType::getEnumKind(), argumentsSet('FFICTypeEnumKind'));
+
+    expectedReturnValues(\FFI\CType::getAttributes(), argumentsSet('FFICTypeAttr'));
+
+    expectedReturnValues(\FFI\CType::getFuncABI(), argumentsSet('FFICTypeAbi'));
 
     override(\FFI::addr(), type(0));
 }
