27221288d3880c9f20a7f6890b77bb03529a0913
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-07-26 17:04:56 +0200

    add test that all stubs present in map

---
+++ tests/StubsStructureTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace StubTests;
+
+
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+use StubTests\TestData\Providers\Stubs\PhpCoreStubsProvider;
+
+class StubsStructureTest extends BaseStubsTest
+{
+
+    /**
+     * @dataProvider \StubTests\TestData\Providers\Stubs\StubsTestDataProviders::stubsDirectoriesProvider
+     */
+    public function testStubsDirectoryExistInMap($directory)
+    {
+        self::assertContains($directory,
+            iterator_to_array(new RecursiveIteratorIterator(
+                new RecursiveArrayIterator(PhpCoreStubsProvider::$StubDirectoryMap)), false),
+            "Stubs directories provider doesn't contain '$directory'. Please add '$directory' to 'PhpCoreStubsProvider::\%StubDirectoryMap'");
+    }
+}
--- tests/TestData/Providers/Stubs/StubsTestDataProviders.php
+++ tests/TestData/Providers/Stubs/StubsTestDataProviders.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace StubTests\TestData\Providers\Stubs;
 
+use DirectoryIterator;
 use Generator;
 use StubTests\Model\PHPFunction;
 use StubTests\TestData\Providers\PhpStormStubsSingleton;
@@ -28,9 +29,21 @@ class StubsTestDataProviders
     public static function coreFunctionsProvider(): ?Generator
     {
         $allFunctions = PhpStormStubsSingleton::getPhpStormStubs()->getFunctions();
-        $coreFunctions = array_filter($allFunctions, fn (PHPFunction $function): bool => $function->stubBelongsToCore === true);
+        $coreFunctions = array_filter($allFunctions, fn(PHPFunction $function): bool => $function->stubBelongsToCore === true);
         foreach ($coreFunctions as $coreFunction) {
             yield "function $coreFunction->name" => [$coreFunction];
+        }
+    }
+
+    public static function stubsDirectoriesProvider(): ?Generator
+    {
+        $stubsDirectory = dirname(__DIR__, 4);
+        foreach (new DirectoryIterator($stubsDirectory) as $directory) {
+            $directoryName = $directory->getBasename();
+            if ($directory->isDot() || !$directory->isDir() || in_array($directoryName, ['tests', 'meta', 'vendor'], true) || str_starts_with($directoryName, '.')) {
+                continue;
+            }
+            yield "directory $directoryName" => [$directoryName];
         }
     }
 }
