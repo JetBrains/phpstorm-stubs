20d0114dac03956512c6e99882b441a0bdb8a11d
Kirill Smelov <kirill.smelov@jetbrains.com>
2021-07-27 13:12:55 +0300

    WI-61438 Pluralise returned generic

--- Core/Core_c.php
+++ Core/Core_c.php
@@ -36,7 +36,7 @@ interface IteratorAggregate extends Traversable
     /**
      * Retrieve an external iterator
      * @link https://php.net/manual/en/iteratoraggregate.getiterator.php
-     * @return Traversable|TValue An instance of an object implementing <b>Iterator</b> or
+     * @return Traversable|TValue[] An instance of an object implementing <b>Iterator</b> or
      * <b>Traversable</b>
      * @throws Exception on failure.
      */
