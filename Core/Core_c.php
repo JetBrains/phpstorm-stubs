f377e9478081b5494f1147a7897c6ad048b4cfe8
Anton <info@ensostudio.ru>
2021-10-05 19:37:49 +0300

    Update Core_c.php
    key can be string or integer

--- Core/Core_c.php
+++ Core/Core_c.php
@@ -71,7 +71,7 @@ interface Iterator extends Traversable
     /**
      * Return the key of the current element
      * @link https://php.net/manual/en/iterator.key.php
-     * @return string|float|int|bool|null scalar on success, or null on failure.
+     * @return string|int|null scalar on success, or null on failure.
      */
     #[TentativeType]
     public function key(): mixed;
