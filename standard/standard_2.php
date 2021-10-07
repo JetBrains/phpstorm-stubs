c6e098ed55bb1e560f576495583335039c754816
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-10-07 09:39:33 +0200

    [phpstorm-stubs] remove Pure attribute on rand functions that can change global state
    mt_rand both reads and modifies global RNG state internally.  random_int()/random_bytes() would modify OS-level state. gmp_random_* modify random state in GMP. array_rand, shuffle, str_shuffle are the same. They all effectively call mt_rand() internally and change state

--- standard/standard_2.php
+++ standard/standard_2.php
@@ -933,7 +933,6 @@ function proc_nice(int $priority): bool {}
  * @return int A pseudo random value between min
  * (or 0) and max (or getrandmax, inclusive).
  */
-#[Pure(true)]
 function rand(int $min = 0, int $max): int {}
 
 /**
@@ -972,7 +971,6 @@ function getrandmax(): int {}
  * @return int A random integer value between min (or 0)
  * and max (or mt_getrandmax, inclusive)
  */
-#[Pure(true)]
 function mt_rand(int $min = 0, int $max): int {}
 
 /**
