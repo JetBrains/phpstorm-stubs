857ab5d195ca5d9b9d97765d8fccc580b3cdc6a1
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-11-24 23:35:19 +0100

    [phpstorm-stubs] cleanup

--- tests/Tools/ModelAutoloader.php
+++ tests/Tools/ModelAutoloader.php
@@ -7,9 +7,8 @@ class ModelAutoloader
     public static function register()
     {
         spl_autoload_register(function ($class) {
-            $file = str_replace('StubTests\\', DIRECTORY_SEPARATOR, $class).'.php';
-            $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
-            $file = __DIR__ . '/..' . $file;
+            $file = str_replace(['StubTests\\', '\\'], DIRECTORY_SEPARATOR, $class).'.php';
+            $file = __DIR__ . '/../' . $file;
             if (file_exists($file)) {
                 require $file;
                 return true;
