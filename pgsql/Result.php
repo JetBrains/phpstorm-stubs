b26b02c4f55b96d5f37ad0a98fe48478db05e23a
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-12-17 17:41:51 +0100

    [phpstorm-stubs] WI-64559 update stubs for pgsql

---
+++ pgsql/Result.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PgSql;
+
+/**
+ * A fully opaque class which replaces a pgsql result resource as of PHP 8.1.0.
+ * @since 8.1
+ * @link https://www.php.net/manual/en/class.pgsql-result.php
+ */
+final class Result {}
