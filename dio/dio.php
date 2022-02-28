c691e0077829447c54445f913db7e26a8921e488
Fernando Herrero <alphp@users.noreply.github.com>
2022-02-28 04:44:14 +0100

    Add missing methods and constants of DIO
    Add missing methods dio_raw() and dio_serial().
    Add missing constants.

--- dio/dio.php
+++ dio/dio.php
@@ -12,6 +12,8 @@
  * dio_tcsetattr — Sets terminal attributes and baud rate for a serial port
  * dio_truncate — Truncates file descriptor fd to offset bytes
  * dio_write — Writes data to fd with optional truncation at length
+ * dio_raw - Opens a raw direct IO stream.
+ * dio_serial - Opens a serial direct IO stream.
  */
 
 /**
@@ -143,3 +145,35 @@ function dio_truncate($fd, int $offset) {}
  * @return int Returns the number of bytes written to fd.
  */
 function dio_write($fd, string $data, int $len = 0) {}
+
+/**
+ * dio_raw ( string filename , string mode [, array options] ) : ?resource
+ * dio_raw - Opens a raw direct IO stream.
+ * @param string $filename The pathname of the file to open.
+ * @param string $mode The mode parameter specifies the type of access you require to the stream (as *fopen()*).
+ * @param array|null $options The currently available options are:
+ * 'data_rate' - baud rate of the port - can be 75, 110, 134, 150, 300, 600, 1200, 1800, 2400, 4800, 7200, 9600, 14400, 19200, 38400, 57600, 115200, 56000, 128000 or 256000 default value is 9600.
+ * 'data_bits' - data bits - can be 8, 7, 6 or 5. Default value is 8.
+ * 'stop_bits' - stop bits - can be 1 or 2. Default value is 1.
+ * 'parity' - can be 0, 1 or 2. Default value is 0.
+ * 'flow_control' - can be 0 or 1. Default value is 1.
+ * 'is_canonical' - can be 0 or 1. Default value is 1.
+ * @return resource|null A stream resource or null on error.
+ */
+function dio_raw(string $filename, string $mode, ?array $options) : ?resource {}
+
+/**
+ * dio_serial ( string $filename , string $mode [, array $options = null] ) : ?resource
+ * dio_serial - Opens a serial direct IO stream.
+ * @param string $filename The pathname of the file to open.
+ * @param string $mode The mode parameter specifies the type of access you require to the stream (as *fopen()*).
+ * @param array|null $options The currently available options are:
+ * 'data_rate' - baud rate of the port - can be 75, 110, 134, 150, 300, 600, 1200, 1800, 2400, 4800, 7200, 9600, 14400, 19200, 38400, 57600, 115200, 56000, 128000 or 256000 default value is 9600.
+ * 'data_bits' - data bits - can be 8, 7, 6 or 5. Default value is 8.
+ * 'stop_bits' - stop bits - can be 1 or 2. Default value is 1.
+ * 'parity' - can be 0, 1 or 2. Default value is 0.
+ * 'flow_control' - can be 0 or 1. Default value is 1.
+ * 'is_canonical' - can be 0 or 1. Default value is 1.
+ * @return resource|null A stream resource or null on error.
+ */
+function dio_serial(string $filename, string $mode, ?array $options) : ?resource {}
