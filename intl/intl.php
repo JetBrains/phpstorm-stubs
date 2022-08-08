6518ee67301325acd2522c3ffce9a64856b8a9f1
DmitryTronin <dmitry.tronin@jetbrains.com>
2022-08-08 11:58:52 +0200

    muting Transliterator::id wrong readonly

--- intl/intl.php
+++ intl/intl.php
@@ -5,7 +5,6 @@
 use JetBrains\PhpStorm\Deprecated;
 use JetBrains\PhpStorm\ExpectedValues as EV;
 use JetBrains\PhpStorm\Internal\LanguageLevelTypeAware as TypeAware;
-use JetBrains\PhpStorm\Internal\PhpStormStubsElementAvailable;
 use JetBrains\PhpStorm\Internal\PhpStormStubsElementAvailable as ElementAvailable;
 use JetBrains\PhpStorm\Internal\TentativeType;
 use JetBrains\PhpStorm\Pure;
@@ -2360,13 +2359,11 @@ class Transliterator
     public const FORWARD = 0;
     public const REVERSE = 1;
 
+    /**
+     * Starting 8.2 $id is readonly to unlock subclassing it
+     */
     #[TypeAware(['8.1' => 'string'], default: '')]
-    #[PhpStormStubsElementAvailable(to: '8.1')]
     public $id;
-
-    #[TypeAware(['8.2' => 'string'], default: '')]
-    #[PhpStormStubsElementAvailable(from: '8.2')]
-    public readonly $id;
 
     /**
      * (PHP &gt;= 5.4.0, PECL intl &gt;= 2.0.0)<br/>
