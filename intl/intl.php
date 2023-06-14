7d6d52ed7a06424f8c1e4b6b98288d38e14fbd41
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2023-06-14 14:04:24 +0200

    [phpstorm-stubs] update constant values

--- intl/intl.php
+++ intl/intl.php
@@ -6754,8 +6754,8 @@ function intltz_get_id_for_windows_id(string $timezoneId, ?string $region = null
  * @link https://php.net/manual/en/intl.constants.php
  */
 define('INTL_MAX_LOCALE_LEN', 156);
-define('INTL_ICU_VERSION', "72.1");
-define('INTL_ICU_DATA_VERSION', "72.1");
+define('INTL_ICU_VERSION', "73.1");
+define('INTL_ICU_DATA_VERSION', "73.1");
 define('ULOC_ACTUAL_LOCALE', 0);
 define('ULOC_VALID_LOCALE', 1);
 define('GRAPHEME_EXTR_COUNT', 0);
