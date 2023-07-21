319956e140c31a9ca92a23dc4d5bb69f93675ee2
Lars Moelleken <lars@moelleken.org>
2023-07-21 07:41:15 +0200

    curl_multi_init: fix return description
    ref.: https://github.com/php/doc-en/pull/2591

--- curl/curl.php
+++ curl/curl.php
@@ -2594,7 +2594,7 @@ function curl_close(#[LanguageLevelTypeAware(['8.0' => 'CurlHandle'], default: '
 /**
  * Returns a new cURL multi handle
  * @link https://php.net/manual/en/function.curl-multi-init.php
- * @return resource|CurlMultiHandle|false a cURL multi handle resource on success, false on failure.
+ * @return resource|CurlMultiHandle a cURL multi handle resource or object depends on the php version
  */
 #[LanguageLevelTypeAware(['8.0' => 'CurlMultiHandle'], default: 'resource')]
 function curl_multi_init(): CurlMultiHandle {}
