256374cdb59b69fd4400426004909550fb9c9bf3
LolGleb <lolglebik@gmail.com>
2023-08-25 14:02:03 +0200

    [phpstorm-stub] WI-73041 Fixed return type in PHPDoc for substr().

--- standard/standard_1.php
+++ standard/standard_1.php
@@ -499,7 +499,7 @@ function strcoll(string $string1, string $string2): int {}
 function money_format(string $format, float $number): ?string {}
 
 /**
- * Return part of a string
+ * Return part of a string or false on failure. For PHP8.0+ only string is returned
  * @link https://php.net/manual/en/function.substr.php
  * @param string $string <p>
  * The input string.
@@ -559,7 +559,6 @@ function money_format(string $format, float $number): ?string {}
  * $rest = substr("abcdef", -3, -1); // returns "de"
  * ?>
  * </pre>
- * @return string|false the extracted part of string or false on failure.
  */
 #[Pure]
 #[LanguageLevelTypeAware(["8.0" => "string"], default: "string|false")]
