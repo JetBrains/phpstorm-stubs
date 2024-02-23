4d4bdcd2d7bda3d51eebbd3a5cc04855f999bda3
Kristofer Karlsson <karlsson.kristofer@gmail.com>
2024-02-23 15:01:58 +0100

    fix: ArrayObject::offsetGet should return null

--- SPL/SPL.php
+++ SPL/SPL.php
@@ -1572,7 +1572,7 @@ class ArrayObject implements IteratorAggregate, ArrayAccess, Serializable, Count
      * @param TKey $key <p>
      * The index with the value.
      * </p>
-     * @return TValue|false The value at the specified index or false.
+     * @return TValue|null The value at the specified index or null.
      */
     #[TentativeType]
     public function offsetGet(#[LanguageLevelTypeAware(['8.0' => 'mixed'], default: '')] $key): mixed {}
