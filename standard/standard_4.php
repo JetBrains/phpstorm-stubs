81f8fba7be843b0414c2eb0d2176e898879f91d3
Dmitriy Derepko <xepozz@list.ru>
2024-02-27 14:40:25 +0700

    Sync headers_sent
    https://www.php.net/manual/en/function.headers-sent.php

--- standard/standard_4.php
+++ standard/standard_4.php
@@ -728,7 +728,7 @@ function header_remove(?string $name): void {}
  * @return bool headers_sent will return false if no HTTP headers
  * have already been sent or true otherwise.
  */
-function headers_sent(&$filename, &$line): bool {}
+function headers_sent(string &$filename = null, int &$line = null): bool {}
 
 /**
  * Returns a list of response headers sent (or ready to send)
