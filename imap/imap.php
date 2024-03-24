7d4fe939ee65efc20cbefa2a64db593c924f1d5c
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2024-03-24 12:51:35 +0100

    fix param type in imap_headerinfo

--- imap/imap.php
+++ imap/imap.php
@@ -165,7 +165,7 @@ function imap_headers(#[LanguageLevelTypeAware(['8.1' => 'IMAP\Connection'], def
  * </dl>
  */
 function imap_headerinfo(
-    #[LanguageLevelTypeAware(['8.0' => 'IMAP\Connection'], default: 'resource')] $imap,
+    #[LanguageLevelTypeAware(['8.1' => 'IMAP\Connection'], default: 'resource')] $imap,
     int $message_num,
     int $from_length = 0,
     int $subject_length = 0,
