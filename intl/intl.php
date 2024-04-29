80e1e4810b9b72f2ce3871a344762e3a0ea87423
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2024-04-29 14:52:42 +0200

    [phpstorm] WI-75881 add NumberFormatter::DECIMAL to expected values

--- intl/intl.php
+++ intl/intl.php
@@ -925,7 +925,7 @@ class NumberFormatter
     #[Pure]
     public function __construct(
         #[LanguageAware(['8.0' => 'string'], default: '')] $locale,
-        #[LanguageAware(['8.0' => 'int'], default: '')] #[EV([NumberFormatter::PATTERN_DECIMAL,
+        #[LanguageAware(['8.0' => 'int'], default: '')] #[EV([NumberFormatter::DECIMAL, NumberFormatter::PATTERN_DECIMAL,
             NumberFormatter::PATTERN_RULEBASED, NumberFormatter::CURRENCY, NumberFormatter::PERCENT,
             NumberFormatter::SCIENTIFIC, NumberFormatter::SPELLOUT, NumberFormatter::ORDINAL,
             NumberFormatter::DURATION, NumberFormatter::PATTERN_RULEBASED, NumberFormatter::CURRENCY_ACCOUNTING,
