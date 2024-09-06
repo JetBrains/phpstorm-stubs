7481959f546137080c4b084e9a596f84072cda0d
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2024-09-06 13:05:48 +0000

    make explicit nullable types in ExpectedValues attribute

--- meta/attributes/ExpectedValues.php
+++ meta/attributes/ExpectedValues.php
@@ -38,5 +38,5 @@ use Attribute;
 #[Attribute(Attribute::TARGET_FUNCTION|Attribute::TARGET_METHOD|Attribute::TARGET_PARAMETER|Attribute::TARGET_PROPERTY)]
 class ExpectedValues
 {
-    public function __construct(array $values = [], array $flags = [], string $valuesFromClass = null, string $flagsFromClass = null) {}
+    public function __construct(array $values = [], array $flags = [], ?string $valuesFromClass = null, ?string $flagsFromClass = null) {}
 }
