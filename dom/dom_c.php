2b34e5b36cf296d0299e9f046fbe12cef0db09c1
Alessandro Lai <alessandro.lai85@gmail.com>
2025-05-27 15:13:37 +0200

    Revert #1741 - \DOMElement::$attributes is never null
    See https://github.com/phpstan/phpstan/issues/13076

--- dom/dom_c.php
+++ dom/dom_c.php
@@ -1720,7 +1720,7 @@ class DOMElement extends DOMNode implements DOMParentNode, DOMChildNode
     public $nextSibling;
 
     /**
-     * @var DOMNamedNodeMap<DOMAttr>|null
+     * @var DOMNamedNodeMap<DOMAttr>
      * A <classname>DOMNamedNodeMap</classname> containing the attributes of this node (if it is a <classname>DOMElement</classname>) or NULL otherwise.
      * @link https://php.net/manual/en/class.domnode.php#domnode.props.attributes
      */
