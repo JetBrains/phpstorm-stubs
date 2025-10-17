2420347bf632b09deeecba30b1784f9b5422bce4
Christian Sciberras <christian@sciberras.me>
2025-10-18 01:31:35 +0200

    Fix env var; tweak workflow

--- runTests.sh
+++ runTests.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Installing composer packages..."
-docker-compose -f docker-compose.yml run test_runner composer install --ignore-platform-reqs
+docker-compose -f docker-compose.yml run test_runner composer install
 
-echo "Checking stub map..."
-docker-compose -f docker-compose.yml run test_runner vendor/bin/phpunit --testsuite Check_Stub_Map
+echo "Checking PhpDoc..."
+docker-compose -f docker-compose.yml run test_runner vendor/bin/phpunit --testsuite PhpDoc
+
+echo "Checking Structure..."
+docker compose -f docker-compose.yml run test_runner vendor/bin/phpunit --testsuite Structure
 
 allPhpVersions=("7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2" "8.3" "8.4" "8.5")
 phpVersions=("$@")
@@ -19,13 +22,16 @@ do
   cd "$SCRIPT_DIR" || exit
 
   echo "Building docker container for PHP_$i..."
-  docker-compose -f docker-compose.yml build
+  docker compose -f docker-compose.yml build --build-arg "PHP_VERSION=$i"
+
+  echo "Installing composer packages..."
+  docker compose -f docker-compose.yml run test_runner composer install
 
   echo "Dumping reflection data to file $SCRIPT_DIR/ReflectionData.dat for PHP_$i..."
-  docker-compose -f docker-compose.yml run -e PHP_VERSION="$i" php_under_test /usr/local/bin/php tests/Tools/dump-reflection-to-file.php ReflectionData.dat
+  docker compose -f docker-compose.yml run -e "PHP_VERSION=$i" php_under_test /usr/local/bin/php tests/Tools/dump-reflection-to-file.php ReflectionData.dat
 
   echo "Running tests against PHP_$i..."
-  docker-compose -f docker-compose.yml run -e PHP_VERSION="$i" test_runner vendor/bin/phpunit --testsuite PHP_"$i"
+  docker compose -f docker-compose.yml run -e "PHP_VERSION=$i" test_runner vendor/bin/phpunit --testsuite "PHP_$i"
 
   echo "Removing file $SCRIPT_DIR/ReflectionData.dat with reflection data for PHP_$i..."
   rm -f "$SCRIPT_DIR/ReflectionData.dat"
