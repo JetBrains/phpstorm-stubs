0ec1ed7faabd5fca94df54f88c1f5e2c3442788f
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-10-26 16:38:30 +0100

    Fix getAttributeNode return type

--- dom/dom_c.php
+++ dom/dom_c.php
@@ -1817,7 +1817,7 @@ class DOMElement extends DOMNode implements DOMParentNode, DOMChildNode
      * @param string $qualifiedName <p>
      * The name of the attribute.
      * </p>
-     * @return DOMAttr The attribute node.
+     * @return DOMAttr|false The attribute node or false if there is none.
      */
     public function getAttributeNode(#[LanguageLevelTypeAware(['8.0' => 'string'], default: '')] $qualifiedName) {}
 
